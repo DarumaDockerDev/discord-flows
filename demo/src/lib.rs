baf652052b45b27e328ed1d41f4da5d94af79650
Jinser <cmdr.jv@gmail.com>
2023-04-28 15:53:46 +0800

    only action

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,20 +1,38 @@
-use discord_flows::{get_client, listen_to_event, model::Message};
+use std::env;
+
+use discord_flows::get_client;
+use slack_flows::{listen_to_channel, SlackMessage};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let token = env::var("DISCORD_TOKEN").unwrap();
 
-    listen_to_event(token.clone(), move |msg| handle(msg, token)).await;
+    let team_name = env::var("team").unwrap_or("ham-5b68442".to_string());
+    let channel_name = env::var("channel").unwrap_or("general".to_string());
+
+    // listen_to_event(token.clone(), move |msg| handle(msg, token)).await;
+    listen_to_channel(&team_name, &channel_name, |msg| cb(msg, &token));
 }
 
-async fn handle(msg: Message, token: String) {
+fn cb(msg: SlackMessage, token: &str) {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(handle(msg, token))
+}
+
+async fn handle(msg: SlackMessage, token: &str) {
     let client = get_client(token);
-    let channel_id = msg.channel_id;
-    let content = msg.content;
+
+    let channel_id = 1097913977058627707;
+    let content = msg.text;
+
     _ = client
         .send_message(
-            channel_id.into(),
+            channel_id,
             &serde_json::json!({
                 "content": content,
             }),
